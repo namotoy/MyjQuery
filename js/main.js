71d0bbbacf5c1cdb6df2650cdd173d13a97693c7
namotoy <namotopet0615@gmail.com>
2022-07-19 17:07:18 +0900

    複数要素の処理

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@
 
 $(()=>{
  $('button').click(()=>{
-    $('p').text('GOOD MORNING');
- })
+    // $('p').text('GOOD MORNING');
+    // $('p').addClass('red-text');
+    $('p').
+      text('GOOD MORNING!!').
+      addClass('red-text');
+    // $('li')
+    //   .eq(2)
+    //   .addClass('red-text');
+
+    $('li:nth-child(odd),li:last-child')
+      .addClass('red-text');
+ });
 
 });
