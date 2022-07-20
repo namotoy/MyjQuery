f908b7d62440be9a9c78e5762998a16a4d504de3
namotoy <namotopet0615@gmail.com>
2022-07-20 12:57:46 +0900

    削除ボタンの設定

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 $(()=>{
+const $input = $('input');
+$input.focus();
+
  $('button').click(()=>{
     // $('p').text('GOOD MORNING');
     // $('p').addClass('red-text');
@@ -15,11 +18,18 @@ $(()=>{
       .addClass('red-text');
     
     $('<li>')
-    .text($('input').val())
+    .text($input.val())
     .appendTo('ul');
-    $('input')
+    $input
      .val('')
      .focus();
  });
 
+ $('ul').click(e =>{
+  if(e.target.nodeName !== 'LI' || !confirm('本当に削除しますか？')){
+    return;
+  }
+  // e.target.remove();
+  $(e.target).fadeOut();
+ });
 });
